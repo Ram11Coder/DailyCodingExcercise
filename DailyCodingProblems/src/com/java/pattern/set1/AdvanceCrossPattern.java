3860e18c5b8e39fdef9fe66c6f6efac77c50f545
thula <thula@LAPTOP-O17H34QQ>
2021-05-21 00:22:09 +0530

    Added cross pattern problems

---
+++ DailyCodingProblems/src/com/java/pattern/set1/AdvanceCrossPattern.java
@@ -0,0 +1,22 @@
+package com.java.pattern.set1;
+
+import java.util.Scanner;
+
+public class AdvanceCrossPattern {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+
+			for (int j = 0; j < 2 * n - 1; j++) {
+				if (j == i || j == n - i - 1 || (j == n + i - 1 && i != 0) || (j == 2 * n - i - 2 && i != n - 1))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+
+			}
+			System.out.println();
+		}
+	}
+}
