d75834d14558903a0f8c41e0922e02ff6b968fe7
Ram11Coder <rambeliver11@gmail.com>
2021-06-07 23:21:22 +0530

    Added the few star and number patterns

---
+++ DailyCodingProblems/src/com/java/pattern/set1/HollowStar.java
@@ -0,0 +1,23 @@
+package com.java.pattern.set1;
+
+import java.util.Scanner;
+
+public class HollowStar {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number :");
+		int n = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+
+			for (int j = 0; j < n; j++) {
+				if (i == 0 || j == 0 || j == n - 1 || i == n - 1) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
