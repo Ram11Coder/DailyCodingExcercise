57757480659c9a235070a2344e76aac4621b9d1a
Ram11Coder <rambeliver11@gmail.com>
2021-06-12 12:52:30 +0530

    Added the mountain and pascal pattern

---
+++ DailyCodingProblems/src/com/java/pattern/set1/MountainPattern.java
@@ -0,0 +1,30 @@
+package com.java.pattern.set1;
+
+import java.util.Scanner;
+
+public class MountainPattern {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int j = 0;
+		for (int i = 0; i < n; i++) {
+
+			for (int c = 0; c < n; c++) {
+				if (c == 0) {
+					for (j = 0; j < n - 1 - i; j++) {
+						System.out.print(" ");
+					}
+				} else {
+					for (int j1 = 0; j1 < 2 * j; j1++)
+						System.out.print(" ");
+				}
+				for (int k = 0; k < 2 * i + 1; k++)
+					System.out.print("*");
+
+			}
+			System.out.println();
+		}
+
+	}
+}
