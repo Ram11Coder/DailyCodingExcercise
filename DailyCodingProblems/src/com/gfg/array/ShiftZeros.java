351db2af64c340277235ff3ef88e1dbd556c7a5a
Ram11Coder <rambeliver11@gmail.com>
2021-11-05 17:58:35 +0530

    Added daily coding problems of array

---
+++ DailyCodingProblems/src/com/gfg/array/ShiftZeros.java
@@ -0,0 +1,30 @@
+package com.gfg.array;
+
+import java.util.Arrays;
+/**
+ * 
+ * Shift all zeros in to right side
+ *
+ */
+public class ShiftZeros {
+	public static void main(String[] args) {
+
+		int arr[] = { 1, 0, 2, 0, 3, 0, 0, 0, 0 };
+
+
+		System.out.println(Arrays.toString(shiftAllZeros(arr)));
+	}
+
+	private static int[] shiftAllZeros(int[] arr) {
+
+		int newArr[] = new int[arr.length];
+		int count = 0;
+		for (int n : arr) {
+			if (n != 0)
+				newArr[count++] = n;
+		}
+
+		return newArr;
+	}
+
+}
