5a2ea9edcaa690f30efe34ae28d5b883477b89ce
Ram11Coder <rambeliver11@gmail.com>
2021-11-10 18:37:09 +0530

    Committed daily coding question of strings

---
+++ DailyCodingProblems/src/com/gfg/string/StringRange.java
@@ -0,0 +1,47 @@
+package com.gfg.string;
+
+/**
+ * First Occurrences Given two strings, find the first occurrence of all
+ * characters of second string in the first string and print the characters
+ * between the least and the highest index I/P: ZOHOCORPORATION PORT O/P:
+ * OHOCORPORAT Explanation: The index of P in first string is 7, O is 1, R is 6
+ * and T is 11. The largest range is 1 – 11. So print the characters of the
+ * first string in this index range i.e. OHOCORPORAT
+ */
+public class StringRange {
+
+	public static void main(String[] args) {
+
+		printRange("ZOHOCORPORATION", "PORT");
+		printRange("ROCKY BHAI", "BAD");
+		printRange("ZOHOCORPORATION", "");
+		printRange("ZOHOCORPORATION", " ");
+
+		printRange("ZOHOCORPORATION", null);
+		printRange("HELLO WORLD", "OLD");
+		printRange("I LOVE JAVA PROGRAMMING", "CODE");
+		printRange("I LOVE JAVA PROGRAMMING", "code");
+
+	}
+
+	private static void printRange(String word, String key) {
+		if (key == null || key.isBlank()) {
+			System.out.println("No matching char in the string ");
+			return;
+		}
+		word = word.toUpperCase();
+		key = key.toUpperCase();
+		int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+		for (int i = 0; i < key.length(); i++) {
+			int index = word.indexOf(key.charAt(i));
+			if (index != -1) {
+				if (index > max)
+					max = index;
+				if (index < min)
+					min = index;
+			}
+		}
+		System.out.println(word.substring(min, max + 1));
+	}
+
+}
---
+++ DailyCodingProblems/src/com/gfg/string/StringTransformation.java
@@ -0,0 +1,59 @@
+package com.gfg.string;
+
+import java.util.Arrays;
+
+/**
+ * Replace the characters in a string based on the transformation:
+ *  A -> Z, B->Y,C->X and so on
+ *  Input: FADE
+ *  Output: UZWV
+ *
+ */
+public class StringTransformation {
+
+	public static void main(String[] args) {
+		transform("FADE");
+		transform("ABCD");
+		transform("ZXYZ");
+		transform("HAPPY");
+		transform("CODING");
+	}
+
+	private static void transform(String word) {
+
+		approach1(word);
+		/*
+		 * approach2(word); 
+		 * approach3(word);
+		 */
+
+	}
+
+	private static void approach3(String word) {
+		for (int i = 0; i < word.length(); i++) {
+			char c = (char) (65 + (90 - word.charAt(i)));
+			System.out.print(c);
+		}
+		System.out.println();
+	}
+
+	private static void approach2(String word) {
+		char c[] = word.toCharArray();
+		for (int i = 0; i < word.length(); i++)
+			c[i] = (char) ('A' + ('Z' - word.charAt(i)));
+
+		System.out.print(Arrays.toString(c));
+		System.out.println();
+	}
+
+	private static void approach1(String word) {
+
+		for (int i = 0; i < word.length(); i++) {
+			char c = (char) ('A' + ('Z' - word.charAt(i)));
+			System.out.print(c);
+		}
+		System.out.println();
+
+	}
+
+}
