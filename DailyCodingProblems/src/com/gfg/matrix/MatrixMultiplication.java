0b0ec02c2ea16e52b66bed5c8f06ec5c15f6a872
Ram11Coder <rambeliver11@gmail.com>
2021-11-14 23:46:16 +0530

    added basic matrix operations

--- DailyCodingProblems/src/com/gfg/matrix/MatrixMultiplication.java
+++ DailyCodingProblems/src/com/gfg/matrix/MatrixMultiplication.java
@@ -1,10 +1,13 @@
 package com.gfg.matrix;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MatrixMultiplication {
+	static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+
 		System.out.println("Enter the 1st row");
 		int row1 = sc.nextInt();
 		System.out.println("Enter the 1st col");
@@ -13,15 +16,19 @@ public class MatrixMultiplication {
 		int row2 = sc.nextInt();
 		System.out.println("Enter the 2nd col");
 		int col2 = sc.nextInt();
-	
-		int mat1[][] = new int[row1][col1];
-		int mat2[][] = new int[row2][col2];
+
+		matrixMultiplication(row1, col1, row2, col2);
+	}
+
+	private static void matrixMultiplication(int row1, int col1, int row2, int col2) {
 
 		if (col1 != row2) {
-			System.out.println("Indefine");
+			System.out.println("undefined");
+			return;
 		}
-
-		int matOperation[][] = new int[row1][col2];
+		int mat1[][] = new int[row1][col1];
+		int mat2[][] = new int[row2][col2];
+		int product[][] = new int[row1][col2];
 		System.out.println("Enter the first matrix");
 		for (int i = 0; i < row1; i++)
 			for (int j = 0; j < col1; j++)
@@ -31,12 +38,24 @@ public class MatrixMultiplication {
 			for (int j = 0; j < col2; j++)
 				mat2[i][j] = sc.nextInt();
 
-		System.out.println("*******Matric multiplication*********");
-		
-		for (int i = 0; i < row1; i++) {
-			for (int j = 0; j <col2; j++) {
-				matOperation[i][j]=(mat1[][]*mat2[][])+(mat1[][]*mat2[][]);
-			}
+		for (int i = 0; i < row1; i++)
+			for (int j = 0; j < col2; j++)
+				for (int k = 0; k < col1; k++)
+					product[i][j] += mat1[i][k] * mat2[k][j];
+
+		System.out.println("Matrix 1");
+		for (int m[] : mat1) {
+			System.out.println(Arrays.toString(m));
 		}
+		System.out.println("Matrix 2");
+		for (int m[] : mat2) {
+			System.out.println(Arrays.toString(m));
+		}
+		System.out.println("Matric multiplication");
+
+		for (int m[] : product) {
+			System.out.println(Arrays.toString(m));
+		}
+
 	}
 }
---
+++ DailyCodingProblems/src/com/gfg/matrix/TransposeMatrix.java
@@ -0,0 +1,30 @@
+package com.gfg.matrix;
+
+import java.util.Arrays;
+
+/**
+ * 
+ * Transpose matrix contains first matrix row as column and col as row
+ *
+ */
+public class TransposeMatrix {
+
+	public static void main(String[] args) {
+		int arr[][] = new int[][] { { 1, 2, 3, }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } };
+		for (int[] m : arr) {
+			System.out.println(Arrays.toString(m));
+		}
+
+		int transopose[][] = new int[arr[0].length][arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				transopose[j][i] = arr[i][j];
+			}
+		}
+		System.out.println();
+		for (int[] m : transopose) {
+			System.out.println(Arrays.toString(m));
+		}
+	}
+
+}
