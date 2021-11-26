25e5946a6e045b85ba504e95b426d45ff51a7df7
Ram11Coder <rambeliver11@gmail.com>
2021-11-26 22:24:04 +0530

    Planned for weekend problems

---
+++ DailyCodingProblems/src/com/gfg/matrix/MatrixAddition.java
@@ -0,0 +1,31 @@
+package com.gfg.matrix;
+
+/**
+ * Matrix Addition Given n integer arrays of different size, find the addititon
+ * of numbers represented by the arrays
+ * 
+ * I/P: 4
+ * 
+ * 3 5 4 2
+ * 
+ * 2 4 5
+ * 
+ * 4 5 6 7 8
+ * 
+ * 4 9 2 1
+ * 
+ * 1 2
+ * 
+ * O/P: 50856
+ *
+ * 
+ */
+
+public class MatrixAddition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DailyCodingProblems/src/com/gfg/string/AdjacentPair.java
@@ -0,0 +1,15 @@
+package com.gfg.string;
+
+/**
+ * Two strings of equal length will be given. Print all the adjacent pairs which
+ * are not equal.
+ * 
+ * Input: asdfghij and adsfgijh
+ * 
+ * Output: sd-ds, hij-ijh
+ * 
+ *
+ */
+public class AdjacentPair {
+
+}
