119124056ac4058edd6dfd29a454e7fb2588021a
Ram11Coder <rambeliver11@gmail.com>
2022-11-03 12:20:31 +0530

    Added additional solution

--- DailyCodingProblems/src/patterns/DoublesideStaircase.java
+++ DailyCodingProblems/src/patterns/DoublesideStaircase.java
@@ -5,6 +5,27 @@ public class DoublesideStaircase {
 	public static void main(String[] args) {
 		int n = 10, c = 1;
 
+		approach1(n, c);
+		approach2(n / 2);
+	}
+
+	private static void approach2(int n) {
+		for (int i = 1, c = 0; i <= 2 * n; i++) {
+
+			for (int j = 0; j < n - c - 1; j++)
+				System.out.print(" ");
+
+			for (int k = 0; k <= c; k++)
+				System.out.print("**");
+
+			if (i % 2 == 0)
+				c++;
+			System.out.println();
+		}
+
+	}
+
+	private static void approach1(int n, int c) {
 		for (int i = 1; i <= n; i++) {
 
 			for (int j = 1; j <= n / 2 - c; j++)
@@ -16,6 +37,7 @@ public class DoublesideStaircase {
 			if (i % 2 == 0)
 				c++;
 			System.out.println();
+
 		}
 	}
 
