6daeb09fe23fea5eeed7fb5d8899eea47e0f8b4f
Ram11Coder <rambeliver11@gmail.com>
2022-11-03 11:49:56 +0530

    Simplied the code

--- DailyCodingProblems/src/patterns/DiamondShapepattern.java
+++ DailyCodingProblems/src/patterns/DiamondShapepattern.java
@@ -1,48 +1,43 @@
 package patterns;
 
 import java.util.Scanner;
-/**
- * 
- * 
- 
-n= 5
- 
+
+/*
+n=5
+
     *
    ***
   *****
  *******
 *********
  *******
-  *****
-   ***
+  ***** 
+   *** 
     *
 
-
-
- */
+*/
 public class DiamondShapepattern {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		for (int i = 0; i < 2 * n - 1; i++) {
+		int k = 2 * n - 1;
+		for (int i = 0; i < k; i++) {
 			if (i < n) {
-				for (int j = 0; j < n - i - 1; j++) {
+				for (int j = 0; j < n - i - 1; j++)
+					System.out.print(" ");
 
-					System.out.print(" ");
-				}
-				for (int j = 0; j < 2 * i + 1; j++) {
+				for (int j = 0; j < 2 * i + 1; j++)
 					System.out.print("*");
-				}
+
 			} else {
-				for (int j = n; j <= i; j++) {
+				for (int j = n; j <= i; j++)
 					System.out.print(" ");
-				}
-				for (int j = 2 * n - 1; j >= 2 * ((i + 1) - n) + 1; j--) {
+
+				for (int j = k; j > 2 * (i - n + 1); j--)
 					System.out.print("*");
-				}
 			}
 			System.out.println();
 		}
-sc.close();
+		sc.close();
 	}
 }
