39dcd63179473f74bbaf0c568a683d019aee561b
Ram11Coder <rambeliver11@gmail.com>
2022-11-05 13:19:28 +0530

    Added additional approach

--- DailyCodingProblems/src/patterns/RightPascalTriangle.java
+++ DailyCodingProblems/src/patterns/RightPascalTriangle.java
@@ -21,6 +21,12 @@ public class RightPascalTriangle {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		Approach1(n);
+		Approach2(n);
+		sc.close();
+	}
+
+	private static void Approach1(int n) {
 		for (int i = 1; i <= 2 * n - 1; i++) {
 			if (i <= n) {
 				for (int j = 1; j <= i; j++)
@@ -31,6 +37,18 @@ public class RightPascalTriangle {
 			}
 			System.out.println();
 		}
-		sc.close();
+	}
+
+	private static void Approach2(int n) {
+		for (int i = 0; i < 2 * n - 1; i++) {
+			if (i < n) {
+				for (int j = 0; j <= i; j++)
+					System.out.print("* ");
+			} else {
+				for (int j = n; j > i - n + 1; j--)
+					System.out.print("* ");
+			}
+			System.out.println();
+		}
 	}
 }
