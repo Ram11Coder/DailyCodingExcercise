f2265c0c79df8a9ebe58daba03d46c5bbb434b2c
Ram11Coder <rambeliver11@gmail.com>
2022-11-20 22:17:58 +0530

    modified

--- DailyCodingProblems/src/array/KadaneAlgorithm.java
+++ DailyCodingProblems/src/array/KadaneAlgorithm.java
@@ -1,21 +1,20 @@
-package array; 
+package array;
 
 /**
- * iven an array Arr[] of N integers. Find the contiguous sub-array(containing
- * at least one number) which has the maximum sum and return its sum.
+ * Given an array Arr[] of N integers. Find the contiguous sub-array(containing at least one number)
+ * which has the maximum sum and return its sum.
  * 
  * 
  * Example 1:
  * 
- * Input: N = 5 Arr[] = {1,2,3,-2,5} Output: 9 Explanation: Max subarray sum is
- * 9 of elements (1, 2, 3, -2, 5) which is a contiguous subarray. Example 2:
+ * Input: N = 5 Arr[] = {1,2,3,-2,5} Output: 9 Explanation: Max subarray sum is 9 of elements (1, 2,
+ * 3, -2, 5) which is a contiguous subarray. Example 2:
  * 
- * Input: N = 4 Arr[] = {-1,-2,-3,-4} Output: -1 Explanation: Max subarray sum
- * is -1 of element (-1)
+ * Input: N = 4 Arr[] = {-1,-2,-3,-4} Output: -1 Explanation: Max subarray sum is -1 of element (-1)
  * 
- * Your Task: You don't need to read input or print anything. The task is to
- * complete the function maxSubarraySum() which takes Arr[] and N as input
- * parameters and returns the sum of subarray with maximum sum.
+ * Your Task: You don't need to read input or print anything. The task is to complete the function
+ * maxSubarraySum() which takes Arr[] and N as input parameters and returns the sum of subarray with
+ * maximum sum.
  * 
  * 
  * Expected Time Complexity: O(N) Expected Auxiliary Space: O(1)
@@ -27,20 +26,22 @@ package array;
  */
 public class KadaneAlgorithm extends Object {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		/*
-		 * Dynamic programming int arr[] = { 1,2,3,-2,5};
-		 */
-		int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
-		int finalMax = Integer.MIN_VALUE;
-		int max = 0;
-		for (int i = 0; i < arr.length; i++) {
-			max = Math.max(arr[i], max + arr[i]);
-			if (finalMax < max)
-				finalMax = max;
-		}
-		System.out.println(finalMax);
-	}
+    /*
+     * Dynamic programming int arr[] = { 1,2,3,-2,5};
+     */
+    // int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+    int arr[] = {-2, -3, 1, 4, -1, -2, 3, 5};
+
+    int finalMax = Integer.MIN_VALUE;
+    int max = 0;
+    for (int i = 0; i < arr.length; i++) {
+      max = Math.max(arr[i], max + arr[i]);
+      if (finalMax < max)
+        finalMax = max;
+    }
+    System.out.println(finalMax);
+  }
 
 }
