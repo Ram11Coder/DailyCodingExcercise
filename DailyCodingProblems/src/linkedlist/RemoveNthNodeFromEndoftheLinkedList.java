405b8087a6384503b5ad8bae5aaf94e95c180801
Ram11Coder <rambeliver11@gmail.com>
2022-11-21 00:00:23 +0530

    added comments

--- DailyCodingProblems/src/linkedlist/RemoveNthNodeFromEndoftheLinkedList.java
+++ DailyCodingProblems/src/linkedlist/RemoveNthNodeFromEndoftheLinkedList.java
@@ -8,7 +8,7 @@ public class RemoveNthNodeFromEndoftheLinkedList {
     SinglyLinkedlist.insertNode(4, head);
     SinglyLinkedlist.insertNode(5, head);
     SinglyLinkedlist.insertNode(6, head);
-    int k = 4;
+    int k = 6;
 
     // SinglyLinkedlist.print(removeNthNode_node(head, k));
     SinglyLinkedlist.print(removeNthNode_optimal(head, k));
@@ -27,20 +27,19 @@ public class RemoveNthNodeFromEndoftheLinkedList {
     dummy.next = head;
     SinglyLinkedlist slowPtr = dummy, fastPtr = dummy;
 
-    for (int i = 1; i <= k; i++) {
+    for (int i = 1; i <= k; i++)
       fastPtr = fastPtr.next;
-    }
+
     while (fastPtr.next != null) {
       fastPtr = fastPtr.next;
       slowPtr = slowPtr.next;
     }
-    if (slowPtr == dummy) {
-      slowPtr.next = slowPtr.next.next;
-      return slowPtr;
-    }
+
+    // Removing the element
     slowPtr.next = slowPtr.next.next;
 
-    return head;
+    // dummy.next will return head
+    return dummy.next;
   }
 
   /**
