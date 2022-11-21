60176f583cee9b8d2dc3717cc1c022c67d4b5514
Ram11Coder <rambeliver11@gmail.com>
2022-11-21 08:14:49 +0530

    Added optimal solution

--- DailyCodingProblems/src/array/Twosum.java
+++ DailyCodingProblems/src/array/Twosum.java
@@ -1,32 +1,62 @@
 package array;
 
-public class Twosum {
-	//I will improve the program optimize in future 
-public static void main(String[] args) {
-	int key =7;
-	int arr[]= {1,3,1,8,4};
-	boolean flag=false;
-		
-/**
- * Best approach
- * 1.Check the number which is near greater than 7
- * 2.Start iterate to till that index
- * 3.find the sum which is equal to 7 then print the true 
- */
-for (int i = 0; i < arr.length; i++) {
-	
-	for (int j = i+1; j < arr.length; j++) {
-		if(arr[i]+arr[j]==key) {
-			flag=true;
-			break;
-		}
-	}
-}
-if(flag==true)
-System.out.println("True");
-else
-	System.out.println("False");
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-}
+public class Twosum {
+  // I will improve the program optimize in future
+  public static void main(String[] args) {
+    int key = 9;
+    int arr[] = {1, 3, 1, 8, 4};
+
+    /**
+     * Bruteforce - 2 iteration
+     *
+     * TC - O(n^2)
+     *
+     * SC - O(1)
+     *
+     */
+
+    System.out.println(Arrays.toString(bruteForce(key, arr)));
+
+    /**
+     * Optimal approach - using hash
+     * 
+     */
+    System.out.println(Arrays.toString(optimal(key, arr)));
+
+  }
+
+  private static int[] optimal(int key, int[] arr) {
+    Map<Integer, Integer> map = new HashMap<>();
+    int result[] = new int[2];
+    for (int i = 0; i < arr.length; i++) {
+      int k = (int) Math.abs(key - arr[i]);
+      if (map.containsKey(k)) {
+        result[1] = i;
+        result[0] = map.get(k);
+        return result;
+      }
+      map.put(arr[i], i);
+    }
+    return null;
+  }
+
+  private static int[] bruteForce(int key, int[] arr) {
+    int result[] = new int[2];
+    for (int i = 0; i < arr.length; i++) {
+      int diff = (int) Math.abs(key - arr[i]);
+      for (int j = i + 1; j < arr.length; j++) {
+        if (arr[j] == diff) {
+          result[1] = j;
+          result[0] = i;
+          return result;
+        }
+      }
+    }
+    return result;
+  }
 
 }
