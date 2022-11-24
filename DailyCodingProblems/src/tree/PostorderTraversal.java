bbbea8d7412c4306be53144a3821f662c39025df
Ram11Coder <rambeliver11@gmail.com>
2022-11-24 15:02:50 +0530

    Added iterative sol of postorder

--- DailyCodingProblems/src/tree/PostorderTraversal.java
+++ DailyCodingProblems/src/tree/PostorderTraversal.java
@@ -1,5 +1,9 @@
 package tree;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class PostorderTraversal {
   public static void main(String[] args) {
     NodeTree root = new NodeTree(1);
@@ -13,15 +17,79 @@ public class PostorderTraversal {
     root.right.right.left = new NodeTree(9);
     root.right.right.right = new NodeTree(10);
 
-    postorderTraversal(root);
+    System.out.println("Recursive : ");
+    postorderTraversal_recursive(root);
+
+    System.out.println("\nIterative using 2 stacks : ");
+    postorderTraversal_using2stacks(root);
+
+    System.out.println("\nIterative using 1 stacks : ");
+    postorderTraversal_using1stack(root);
+
   }
 
-  private static void postorderTraversal(NodeTree root) {
+  private static void postorderTraversal_using1stack(NodeTree root) {
+
+    Stack<NodeTree> st = new Stack<>();
+    NodeTree curr = root;
+    List<Integer> list = new ArrayList<Integer>();
+    while (curr != null || !st.isEmpty()) {
+
+      if (curr != null) {
+        st.push(curr);
+        curr = curr.left;
+      } else {
+        NodeTree temp = st.peek().right;
+
+        if (temp == null) {
+          temp = st.peek();
+          st.pop();
+          list.add(temp.data);
+          while (!st.isEmpty() && temp == st.peek().right) {
+            temp = st.peek();
+            st.pop();
+            list.add(temp.data);
+          }
+        } else {
+          curr = temp;
+        }
+      }
+    }
+    System.out.println(list);
+  }
+
+  private static void postorderTraversal_using2stacks(NodeTree root) {
+
+    Stack<NodeTree> st1 = new Stack<NodeTree>();
+
+    Stack<NodeTree> st2 = new Stack<NodeTree>();
+    NodeTree node = root;
+    st1.push(node);
+
+    while (!st1.isEmpty()) {
+      node = st1.pop();
+
+      st2.push(node);
+      if (node.left != null) {
+        st1.push(node.left);
+      }
+      if (node.right != null) {
+        st1.push(node.right);
+      }
+
+    }
+
+    while (!st2.isEmpty()) {
+      System.out.print(st2.pop().data + " ");
+    }
+  }
+
+  private static void postorderTraversal_recursive(NodeTree root) {
     if (root == null)
       return;
 
-    postorderTraversal(root.left);
-    postorderTraversal(root.right);
+    postorderTraversal_recursive(root.left);
+    postorderTraversal_recursive(root.right);
     System.out.print(root.data + " ");
   }
 }
