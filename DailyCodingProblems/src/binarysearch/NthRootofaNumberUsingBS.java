3e23b994952a5e093179d97795fffad0f4069ea6
Ram11Coder <rambeliver11@gmail.com>
2022-12-12 23:53:00 +0530

    Added Nth root of number uding BS problem

---
+++ DailyCodingProblems/src/binarysearch/NthRootofaNumberUsingBS.java
@@ -0,0 +1,45 @@
+package binarysearch;
+
+/**
+ * The nth root of a number M is defined as a number X when raised to the power N equals M.
+ * 
+ * Example 1:
+ * 
+ * Input: N=3 M=27
+ * 
+ * Output: 3
+ * 
+ * Explanation: The cube root of 27 is 3. Example 2:
+ * 
+ * Input: N=2 M=16
+ * 
+ * Output: 4
+ * 
+ * Explanation: The square root of 16 is 4 Example 3:
+ * 
+ * Input: N=5 M=243
+ * 
+ * Output: 3
+ * 
+ * Explanation: The 5th root of 243 is 3
+ *
+ */
+public class NthRootofaNumberUsingBS {
+  public static void main(String[] args) {
+    int m = 27, n = 3;
+    getNthRoot(m, n);
+  }
+
+  /**
+   * Time Complexity: N x log(M x 10^d)
+   * 
+   * Space Complexity: O(1)
+   * 
+   * @param m
+   * @param n
+   */
+  private static void getNthRoot(int m, int n) {
+
+   
+  }
+}
