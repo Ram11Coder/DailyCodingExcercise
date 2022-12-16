6783156eeb75f13b51eff976ce1ec42652077406
Ram11Coder <rambeliver11@gmail.com>
2022-12-16 19:44:41 +0530

    Added Best time buy and sell stocks problem

---
+++ DailyCodingProblems/src/array/BesttimeToBuyAndSellStocks.java
@@ -0,0 +1,28 @@
+package array;
+
+/**
+ * Best time to buy and sell the stock with high profit
+ */
+public class BesttimeToBuyAndSellStocks {
+  public static void main(String[] args) {
+
+    int[] stocks = {3, 5, 2, 1};
+
+    System.out.println(buyAndSellStock(stocks));
+
+    // Recursive sol to be added
+  }
+
+  private static int buyAndSellStock(int[] stocks) {
+    if (stocks.length < 2)
+      return 0;
+
+    int profitSoFarHigh = 0;
+    int minStock = stocks[0];
+    for (int i = 1; i < stocks.length; i++) {
+      profitSoFarHigh = Math.max(profitSoFarHigh, stocks[i] - minStock);
+      minStock = Math.min(minStock, stocks[i]);
+    }
+    return profitSoFarHigh;
+  }
+}
