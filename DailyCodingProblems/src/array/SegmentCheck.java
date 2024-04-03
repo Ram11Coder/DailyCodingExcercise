ef2fe44ff5cbbf45fb81a2ef56fecf05d05b47f9
Ram11Coder <ramanthulasi944@gmail.com>
2024-04-04 00:09:38 +0530

    Improved the readability of code

--- DailyCodingProblems/src/array/SegmentCheck.java
+++ DailyCodingProblems/src/array/SegmentCheck.java
@@ -1,5 +1,6 @@
 package array;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*
@@ -14,52 +15,66 @@ import java.util.Scanner;
  * 12, 14, 3, 9} x = 8 k = 2 Output : No
  */
 // find the o(n) method
-public class Array1 {
+public class SegmentCheck {
 
 	public static boolean optimalApproach(int a[], int key, int seg) {
-		boolean flag = false;
+		boolean isSegmentPresent = false;
 		for (int i = 0; i < a.length; i++) {
-			if (a[i] == key) {
-				flag = true;
-			}
+			if (a[i] == key)
+				isSegmentPresent = true;
+
 			if ((i + 1) % seg == 0 || i == a.length - 1) {
-				if (!flag)
-					return flag;
-				flag = false;
+				if (!isSegmentPresent)
+					return isSegmentPresent;
+				isSegmentPresent = false;
 			}
 		}
-
 		return true;
-
 	}
 
 	public static boolean segCheck(int a[], int key, int seg, int val) {
 		int count = 0;
-		for (int i = val; i < (seg + val); i++) {
+		int size = (seg + val < a.length) ? seg + val : a.length;
+		for (int i = val; i < size; i++) 
 			if (a[i] == key && seg > count++)
 				return true;
-		}
+		
 		return false;
 	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int key = 0, seg = 0;
-		boolean check = true;
+
 		int a[] = { 3, 5, 2, 4, 9, 3, 1, 7, 3, 11, 12, 3 };
 
-		System.out.println("Enter the key:");
+		System.out.println("Enter the key value to search :");
 		key = sc.nextInt();
-		System.out.println("Enter the segment:");
+		System.out.println("Enter the segment range to search the key:");
 		seg = sc.nextInt();
+		System.out.println("Array is : " + Arrays.toString(a));
+		//bruteforce(key, seg, a);
+		isAllValidSegment(optimalApproach(a, key, seg));
+		
+		System.out.println("\nSegments: ");
+		for (int pos = 0; pos < a.length; pos += seg)
+			System.out.println(Arrays.toString(Arrays.copyOfRange(a, pos, pos + seg)));
+
+		sc.close();
+	}
+
+	private static void bruteforce(int key, int seg, int[] a) {
+		boolean check = true;
 		for (int i = 0; i < a.length; i += seg) {
 			if (check != segCheck(a, key, seg, i)) {
 				check = false;
 				break;
 			}
 		}
-		System.out.println((check == false) ? "NO" : "YES");
-		System.out.println(optimalApproach(a, key, seg) ? "YES" : "NO");
-		sc.close();
+		isAllValidSegment(check);
+	}
+
+	private static void isAllValidSegment(boolean check) {
+		System.out.println((check == false) ? "Some segments are not valid" : "All segments are valid");
 	}
 }
