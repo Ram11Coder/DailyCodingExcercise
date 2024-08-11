29191700d0429ce7e220636ae969883cf516badb
Ram11Coder <ramanthulasi944@gmail.com>
2024-08-11 22:25:55 +0530

    Added Pattern13

--- DailyCodingProblems/src/patterns/Pattern13.java
+++ DailyCodingProblems/src/patterns/Pattern13.java
@@ -9,4 +9,18 @@ package patterns;
 // * * * *     *
 
 public class Pattern13 {
+    public static void main(String[] args) {
+        int n = 7;
+        int mid = n / 2;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == mid || j == mid || i == 0 && j > mid || j == 0 && i < mid || i == n - 1 && j < mid || j == n - 1 && i > mid) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
