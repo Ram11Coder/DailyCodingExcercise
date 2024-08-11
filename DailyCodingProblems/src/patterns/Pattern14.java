2f7702c4cdacbae8a0af5cebcacaa843660b35c0
Ram11Coder <ramanthulasi944@gmail.com>
2024-08-11 22:30:54 +0530

    Added Pattern14

--- DailyCodingProblems/src/patterns/Pattern14.java
+++ DailyCodingProblems/src/patterns/Pattern14.java
@@ -8,4 +8,18 @@ package patterns;
 // * * * * * * *
 
 public class Pattern14 {
+    public static void main(String[] args) {
+        int n = 7;
+        int mid = n / 2 - 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0 || j == n - 1 || i == mid || i == n - 1 || i < mid && j % 2 == 0) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
