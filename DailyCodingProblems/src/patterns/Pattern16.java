50428009a126a42bd26b36918e24b335a0833c52
Ram11Coder <ramanthulasi944@gmail.com>
2024-08-14 23:49:11 +0530

    Added Pattern16

--- DailyCodingProblems/src/patterns/Pattern16.java
+++ DailyCodingProblems/src/patterns/Pattern16.java
@@ -10,4 +10,26 @@ package patterns;
 //   *   *   *
 
 public class Pattern16 {
+    public static void main(String[] args) {
+        int n = 7;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i % 2 == 0) {
+                    if (j % 2 == 0) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print(" *");
+                    }
+                } else {
+                    if (j % 2 != 0) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("* ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
 }
