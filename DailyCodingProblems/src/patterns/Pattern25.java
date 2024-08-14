f0eb4c63228ceac4ebf94f2ea33c4041345ed9e1
Ram11Coder <ramanthulasi944@gmail.com>
2024-08-15 00:02:26 +0530

    Added Pattern25

--- DailyCodingProblems/src/patterns/Pattern25.java
+++ DailyCodingProblems/src/patterns/Pattern25.java
@@ -8,4 +8,16 @@ package patterns;
 // * * * * *
 
 public class Pattern25 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0 || i == n - 1 || j == n - 1 || j == n - i - 1)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
 }
