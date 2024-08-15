bbc42b8edf16001fc5d5a004dc1d4a356a5580de
Ram11Coder <ramanthulasi944@gmail.com>
2024-08-15 13:57:21 +0530

    Added Pattern18

--- DailyCodingProblems/src/patterns/Pattern18.java
+++ DailyCodingProblems/src/patterns/Pattern18.java
@@ -11,4 +11,18 @@ package patterns;
 //         *
 
 public class Pattern18 {
+    public static void main(String[] args) {
+        int n = 5;
+        int size = 2 * n - 1;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == n - 1 || j == n - 1 || (i < n && j == n - i - 1 || j == n + i - 1) || (i > n - 1 && j == i - n + 1 || j == size - (i - n) - 2)) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
