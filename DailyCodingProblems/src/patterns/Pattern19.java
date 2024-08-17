fa455ef3fec04fc8616f19ddfbfdce5fe1cc2180
Ram11Coder <ramanthulasi944@gmail.com>
2024-08-17 11:13:23 +0530

    Added Pattern19

--- DailyCodingProblems/src/patterns/Pattern19.java
+++ DailyCodingProblems/src/patterns/Pattern19.java
@@ -11,4 +11,17 @@ package patterns;
 // * * * * * * * * *
 
 public class Pattern19 {
+    public static void main(String[] args) {
+        int n = 5;
+        int size = 2 * n - 1;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i < n && j < n - i || j >= n + i - 1 || i >= n && j <= i - n + 1 || j >= size - 2 - (i - n)) {
+                    System.out.print("* ");
+                } else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
 }
