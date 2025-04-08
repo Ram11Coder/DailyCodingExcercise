d70858f0bf9be701328d0e66884aa8f664b84bf7
Ram11Coder <ramanthulasi944@gmail.com>
2025-04-08 23:28:12 +0530

    Added new pattern problems

--- DailyCodingProblems/src/patterns/Pattern15.java
+++ DailyCodingProblems/src/patterns/Pattern15.java
@@ -13,4 +13,20 @@ package patterns;
 
 
 public class Pattern15 {
+    public static void main(String[] args) {
+        int n = 5;
+        int size = n * 2 - 1;
+        int pos = size;
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == 0 || j == 0 || i == pos - 1 - count || j == pos - 1 - count) {
+                    System.out.print("*");
+                } else
+                    System.out.print(" ");
+            }
+            count+=2;
+            System.out.println();
+        }
+    }
 }
--- DailyCodingProblems/src/patterns/Pattern20.java
+++ DailyCodingProblems/src/patterns/Pattern20.java
@@ -12,4 +12,18 @@ package patterns;
 
 
 public class Pattern20 {
+    public static void main(String[] args) {
+        int n = 5;
+        int size = 2 * n - 1;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i < n && j < n - i - 1 || j
+                        >= n + i - 1 || i >= n && j <= i - n + 1 || j >= size - 2 - (i - n)) {
+                    System.out.print("* ");
+                } else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
 }
